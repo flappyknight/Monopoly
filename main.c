b311ec034fe9bd961b9e485e4558ab8cb1d78fa6
Jqz <964408689@qq.com>
2025-01-27 16:04:33 +0800

    design

--- main.c
+++ main.c
@@ -1,4 +1,18 @@
+#pragma once
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "player.h"
+
+Player * initializePlayer(int n) {
+    Player * player = (Player *) malloc(sizeof(Player));
+    for (int i=0; i<n;i++){
+        player->id = i;
+        player->name = (char *) malloc(20);
+    }
+    return player;
+}
+
 
 int main() {
     printf("Hello, World!\n");
---
+++ player.h
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#define INITIAL_MONEY 20000
+#define INITIAL_POSITION 0
+#define INITIAL_STAGNANT false
+#define INITIAL_PURCHASE_PROBABILITY 0.5
+#define INITIAL_PROPERTY NULL
+#define MAX_PLAYERS 4
+#define MAX_NAME_LENGTH 15
+
+const char NAMES[MAX_PLAYERS][MAX_NAME_LENGTH] = {"Jack", "Tom", "Lisa", "John"};
+
+typedef struct {
+    int id;
+    char name[MAX_NAME_LENGTH];
+    bool stagnant;
+    double money;
+    int position;
+    void *property;
+
+    float purchase_probability;
+
+} Player;
+
+void updateMoney(Player *player, double money);
+
+void addProperty(Player *player, void *property);
+
+void removeProperty(Player *player, void *property);
+
+void stepPlayer(Player *player, int steps);
+
+void movePlayer(Player *player, int place);
+
+
